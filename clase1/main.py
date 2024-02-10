c7e29f5444baab20b2673ac9d66cf1eb3e9236e4
Fabio Valois <92942417+Neon18H@users.noreply.github.com>
2024-02-10 16:03:23 +0000

    carro

--- clase1/main.py
+++ clase1/main.py
@@ -1,1 +1,10 @@
-print("primer codigo en python ")
+class MotorElectrico :
+    potencia = None
+    def encender (self):
+       print("Encendiendo motor electrico")
+    def __init__(self)
+       pass
+motor1= MotorElectrico()
+motor1.potencia=1000
+motor1.encender()
+
